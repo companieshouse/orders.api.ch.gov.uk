2dbf5b50c821a24bf60af3cda2d4e4c4ea44bb79
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-07-08 15:50:49 +0100

    Update unit test for BasketRepositoryImpl
    * Use ArgumentCaptor to verify repository update.

--- src/main/java/uk/gov/companieshouse/orders/api/model/BasketData.java
+++ src/main/java/uk/gov/companieshouse/orders/api/model/BasketData.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BasketData {
     private DeliveryDetails deliveryDetails;
@@ -79,4 +80,27 @@ public class BasketData {
 
     @Override
     public String toString() { return new Gson().toJson(this); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BasketData that = (BasketData) o;
+        return enrolled == that.enrolled
+                && Objects.equals(deliveryDetails, that.deliveryDetails)
+                && Objects.equals(etag, that.etag)
+                && Objects.equals(items, that.items)
+                && Objects.equals(kind, that.kind)
+                && Objects.equals(links, that.links)
+                && Objects.equals(totalBasketCost, that.totalBasketCost);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deliveryDetails, etag, items, kind, links, totalBasketCost, enrolled);
+    }
 }
