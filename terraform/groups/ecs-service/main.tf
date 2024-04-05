75ff4c88fa62f9ca5119f668407b4002d18ca248
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2024-04-05 09:55:23 +0100

    fix: updated multilb config

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -34,13 +34,13 @@ module "ecs-service" {
   lb_listener_rule_priority       = local.lb_listener_rule_priority
   lb_listener_paths               = local.lb_listener_paths
   multilb_listeners               = {
-    "pub-api-lb": {
-      load_balancer_arn = data.aws_lb.service_lb.arn
-      listener_arn             = data.aws_lb_listener.service_lb_listener.arn
-    }
     "priv-api-lb": {
-      listener_arn             = data.aws_lb_listener.service_lb_private_listener.arn,
-      load_balancer_arn = data.aws_lb.service_lb_private.arn
+      listener_arn                = data.aws_lb_listener.secondary_lb_listener.arn,
+      load_balancer_arn           = data.aws_lb.secondary_lb.arn
+    }
+    "pub-api-lb": {
+      load_balancer_arn           = data.aws_lb.service_lb.arn
+      listener_arn                = data.aws_lb_listener.service_lb_listener.arn
     }
   }
 
